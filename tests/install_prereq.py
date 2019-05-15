c3d3287c1f400893a2a4c061b28d66d528a4a856
rakeshgm <rgowdege@redhat.com>
2019-05-15 20:59:48 +0530

    moved rhel7 related rpms to seperate rpms, calling this function when rhbuild is not 4 ignoring error code checking for yum update metadata
    Signed-off-by: rakeshgm <rgowdege@redhat.com>

--- tests/install_prereq.py
+++ tests/install_prereq.py
@@ -50,6 +50,8 @@ def install_prereq(ceph, timeout=1800, skip_subscription=False, repo=False, rhbu
         if not skip_subscription:
             setup_subscription_manager(ceph)
             enable_rhel_rpms(ceph,rhbuild)
+            if not rhbuild.startswith('4'):
+                enable_rhel7_rpms(ceph)
         if repo:
             setup_addition_repo(ceph, repo)
         ceph.exec_command(cmd='sudo yum install -y ' + rpm_all_pkgs, long_running=True)
@@ -135,3 +137,10 @@ def enable_rhel_rpms(self, rhbuild):
         for repo in repos:
             self.exec_command(
                 sudo=True, cmd='subscription-manager repos --enable={r}'.format(r=repo),long_running=True)
+            
+def enable_rhel7_rpms(ceph):
+    ceph.exec_command(
+        cmd='sudo subscription-manager repos --enable=rhel-7-server-rpms \
+             --enable=rhel-7-server-optional-rpms \
+             --enable=rhel-7-server-extras-rpms',
+        long_running=True)
